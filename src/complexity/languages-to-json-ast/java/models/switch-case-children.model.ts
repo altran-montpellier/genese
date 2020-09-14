c455d20ef554da42c59227d0b5cdecc6d7d5ad3a
Genese <63037808+geneseframework@users.noreply.github.com>
2020-09-14 15:08:37 +0200

    Revert "Feature java parser switch (#96)"
    This reverts commit 3743db6c6326070f8a00db2e72dde4aa0e81b016.

--- src/complexity/languages-to-json-ast/java/models/switch-case-children.model.ts
+++
@@ -1,7 +0,0 @@
-import { BlockStatements } from "./block-statements.model";
-import { SwitchLabel } from "./switch-label.model";
-
-export class SwitchCaseChildren {
-    switchLabel?: SwitchLabel[] = [new SwitchLabel()];
-    blockStatements?: BlockStatements[] = [new BlockStatements()];
-}
--- src/complexity/languages-to-json-ast/java/models/switch-statement-children.model.ts
+++ src/complexity/languages-to-json-ast/java/models/switch-statement-children.model.ts
@@ -1,11 +1,10 @@
 import { Infos } from "./infos.model";
 import { Expression } from "./expression.model";
-import { SwitchBlock } from "./switch-block.model";
 
 export class SwitchStatementChildren {
     Switch?: Infos[] = [new Infos()];
     LBrace?: Infos[] = [new Infos()];
     expression?: Expression[] = [new Expression()];
     BBrace?: Infos[] = [new Infos()];
-    switchBlock?: SwitchBlock[] = [new SwitchBlock()];
+    switchBlock?: Infos[] = [new Infos()];
 }
